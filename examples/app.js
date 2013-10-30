c715a3b6925b408ce20a532b08a0ab6aba5fe0de
Jim McAndrew <jim@loc8.us>
2013-10-30 09:06:59 -0600

    Make installation easier

--- examples/app.js
+++ examples/app.js
@@ -1,14 +1,14 @@
 var express = require('express'),
-    poiApi = require('poi-api');
-var path = require('path');
+    poiApi = require('poi-api'),
+    path = require('path');
 
 // Set the environment variables
 var app = express();
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 
 // Link to the poiApt
 app.use('/api', poiApi.routes());
-app.use(express.static(path.join(__dirname, '/iD')));
+app.use(express.static(path.join(__dirname, '/node_modules/iD')));
 
 app.listen(app.get('port'));
 console.log('Node.js server listening on port ' + app.get('port'));
