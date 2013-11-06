b49dece5b67bebccc8adbe0525ae6b1bdd46076b
Jim McAndrew <jim@loc8.us>
2013-11-05 17:40:56 -0700

    Create views for ALL as well as CURRENT and add it to the SQL.json file

--- scripts/sql_scripts/views.sql
+++ scripts/sql_scripts/views.sql
@@ -1,4 +1,4 @@
--- VIEWS
+-- CURRENT DATA views
 
 -- Nodes
 DROP VIEW IF EXISTS api_current_nodes;
@@ -13,7 +13,7 @@ SELECT
   changesets.user_id as uid,
   current_nodes.latitude/10000000::float as lat,
   current_nodes.longitude/10000000::float as lon,
-  (SELECT json_agg(tags) from (SELECT k, v FROM node_tags WHERE node_id = current_nodes.id) tags) AS tag
+  (SELECT json_agg(tags) from (SELECT k, v FROM current_node_tags WHERE node_id = current_nodes.id) tags) AS tag
 FROM
   current_nodes
   JOIN changesets
@@ -23,7 +23,7 @@ FROM
 
 -- Ways
 DROP VIEW IF EXISTS api_current_ways;
-CREATE VIEW api_current_ways AS
+CREATE VIEW n AS
 SELECT
   current_ways.id AS id,
   current_ways.visible AS visible,
@@ -89,3 +89,122 @@ FROM
     ON current_relations.changeset_id = changesets.id
     JOIN users
       ON changesets.user_id = users.id;
+
+-- ALL DATA Views
+
+-- Nodes
+DROP VIEW IF EXISTS api_nodes;
+CREATE VIEW api_nodes AS
+SELECT
+  nodes.node_id as id,
+  nodes.visible,
+  nodes.version,
+  nodes.changeset_id as changeset,
+  nodes.timestamp AT TIME ZONE 'UTC' as timestamp,
+  users.display_name as user,
+  changesets.user_id as uid,
+  nodes.latitude/10000000::float as lat,
+  nodes.longitude/10000000::float as lon,
+  (SELECT json_agg(tags) from (SELECT k, v FROM node_tags WHERE node_id = nodes.node_id AND version = nodes.version) tags) AS tag
+FROM
+  nodes
+  JOIN changesets
+    ON nodes.changeset_id = changesets.id
+     JOIN users
+       ON changesets.user_id = users.id;
+
+-- Ways
+DROP VIEW IF EXISTS api_ways;
+CREATE VIEW api_ways AS
+SELECT
+  ways.way_id AS id,
+  ways.visible AS visible,
+  ways.version AS VERSION,
+  ways.changeset_id AS changeset,
+  ways.timestamp AT TIME ZONE 'UTC' AS TIMESTAMP,
+  users.display_name AS USER,
+  changesets.user_id AS uid,
+  (SELECT json_agg(nodes) FROM (
+    SELECT node_id AS ref
+    FROM way_nodes
+    WHERE way_nodes.way_id = ways.way_id
+      AND version = ways.version
+    ORDER BY sequence_id
+    ) nodes
+  ) AS nd,
+  (SELECT json_agg(tags) FROM (
+      SELECT k, v
+      FROM way_tags
+      WHERE way_tags.way_id = ways.way_id AND
+      way_tags.version = ways.version
+    ) tags
+  ) AS tag
+FROM
+  ways
+  JOIN changesets
+    ON ways.changeset_id = changesets.id
+  JOIN users
+    ON changesets.user_id = users.id;
+
+-- Relations
+DROP VIEW IF EXISTS api_relations;
+CREATE VIEW api_relations AS
+SELECT
+  relations.relation_id as id,
+  relations.visible as visible,
+  relations.version as version,
+  relations.changeset_id as changeset,
+  relations.timestamp AT TIME ZONE 'UTC' as timestamp,
+  users.display_name as user,
+  changesets.user_id as uid,
+  (SELECT json_agg(members) FROM (
+    SELECT
+      lower(member_type::text) as type,
+      member_id as ref, 
+      member_role as role
+    FROM
+      relation_members 
+    WHERE
+      relation_members.relation_id = relations.relation_id AND
+      relation_members.version = relations.version
+  ) members) as member,
+  (SELECT json_agg(tags) FROM (
+    SELECT
+      k,
+      v
+    FROM
+      relation_tags
+    WHERE
+      relation_tags.relation_id = relations.relation_id AND
+      relation_tags.version = relations.version
+  ) tags) as tag
+FROM
+  relations
+  JOIN changesets
+    ON relations.changeset_id = changesets.id
+    JOIN users
+      ON changesets.user_id = users.id;
+
+-- Changesets
+DROP VIEW IF EXISTS api_changesets;
+CREATE VIEW api_changesets AS
+SELECT
+  changesets.id as id,
+  users.display_name as user,
+  changesets.user_id as uid,
+  changesets.created_at AT TIME ZONE 'UTC' as created_at,
+  changesets.closed_at AT TIME ZONE 'UTC' as closed_at,
+  (select (closed_at>created_at AND num_changes<=50000) AS open from changesets open where open.id = changesets.id) as open,
+  changesets.min_lat/10000000::float as min_lat,
+  changesets.min_lon/10000000::float as min_lon,
+  changesets.max_lat/10000000::float as max_lat,
+  changesets.max_lon/10000000::float as max_lon,
+  (SELECT json_agg(tags) FROM (
+    SELECT k, v
+    FROM changeset_tags
+    WHERE changeset_id = changesets.id
+  ) tags) as tag
+FROM
+  changesets
+  JOIN users
+    ON changesets.user_id = users.id;
