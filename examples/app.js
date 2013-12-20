a2c6e9266a9da9b02bf998cb76fc6093a5abcafe
Jim McAndrew <jim@loc8.us>
2013-12-20 16:17:22 -0700

    Update the example app

--- examples/app.js
+++ examples/app.js
@@ -1,10 +1,14 @@
 var express = require('express'),
     poiApi = require('poi-api'),
-    path = require('path');
+    path = require('path'),
+    exphbs  = require('express3-handlebars');
 
 // Set the environment variables
 var app = express();
 app.set('port', process.env.PORT || 3000);
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 // Error Logging
 process.on('uncaughtException', function (err) {
@@ -19,6 +23,16 @@ process.on('uncaughtException', function (err) {
   console.log('************************************************');
   console.log('************************************************');
   console.log('************************************************');
+});
+
+// Forward the browse requests
+app.get('/browse/*', function(req, res) {
+  var suffix = '.html';
+  console.log(req.url, req.url.indexOf('/node/'));
+  if (req.url.indexOf('/node/') < 0) {
+    suffix = '/full' + suffix;
+  }
+  res.redirect(req.url.replace('browse', 'api/0.6') + suffix);
 });
 
 // OSM API
