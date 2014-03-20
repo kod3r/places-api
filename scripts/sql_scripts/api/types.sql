613bc34fe2f0058a1878afcc0a4453b18ec084b9
Jim McAndrew <jim@loc8.us>
2014-03-20 16:28:50 -0600

    Update types.sql

--- scripts/sql_scripts/api/types.sql
+++ scripts/sql_scripts/api/types.sql
@@ -7,4 +7,4 @@ DROP TYPE IF EXISTS diffResult CASCADE;
 CREATE TYPE diffResult AS (old_id bigint, new_id bigint, new_version bigint);
 
 -- Allow the creation of UUIDs
-CREATE EXTENSION "uuid-ossp"
+CREATE EXTENSION "uuid-ossp";
