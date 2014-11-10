b0203ee192204902d102b415c2f5c74cb1db6e57
Jim McAndrew <jim@loc8.us>
2014-11-10 16:05:01 -0700

    Clean up body-parser

--- index.js
+++ index.js
@@ -2,34 +2,23 @@ var express = require('express'),
   config = require('./config'),
   apiXapi = require('./lib/apis/xapi'),
   oauth = require('./lib/oauth/paths'),
-  allowXSS = require('./lib/allowXSS'),
-  bodyParser = require('body-parser');
+  allowXSS = require('./lib/allowXSS');
 // Set the environment variables
 //
 exports.routes = function() {
-  var router = express.Router();
-  // parse application/x-www-form-urlencoded
-  router.use(bodyParser.urlencoded({
-    extended: false
-  }));
-  //
-  // // parse application/json
-  router.use(bodyParser.json());
-
-  var poiApp = require('./lib/apiWrapper')(router);
-
+  var router = express.Router(),
+    poiApp = require('./lib/apiWrapper')(router);
   // From http://wiki.openstreetmap.org/wiki/API_v0.6#General_information
-
   // Allow external webpages to read our JavaScript
   allowXSS(router);
 
   // API Calls
   apiXapi.map(function(apiCall) {
-    poiApp.allow(apiCall.method, apiCall.path, '0.6', apiCall.auth, apiCall.process);
+    poiApp.allow(apiCall.method, apiCall.path, '0.6', apiCall.format, apiCall.auth, apiCall.process);
   });
 
   // Overall capabilities
-  poiApp.allow('GET', 'capabilities', null, null, function(req, res) {
+  poiApp.allow('GET', 'capabilities', null, null, null, function(req, res) {
     res.send({
       'api': config.capabilities
     });
