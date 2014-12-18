e16a6be78f5a6f8d71515894715a7f47aee895cb
Jim McAndrew <jim@loc8.us>
2014-12-18 16:40:40 -0700

    Add unit_code

--- scripts/sql_scripts/api/func_upsert_way.sql
+++ scripts/sql_scripts/api/func_upsert_way.sql
@@ -1,3 +1,4 @@
+--upsert_way
 CREATE OR REPLACE FUNCTION upsert_way(
   bigint,
   integer,
@@ -18,11 +19,15 @@ CREATE OR REPLACE FUNCTION upsert_way(
     v_new_version bigint;
     v_user_id bigint;
     v_res boolean;
+    v_uuid_field text;
     v_uuid text;
+    v_unitcode_field text;
+    v_unitcode text;
   BEGIN 
-    -- Set some value
+    -- Set some values
+      v_uuid_field := 'nps:places_uuid';
+      v_unitcode_field := 'nps:unit_code';
       v_timestamp := now();
-      v_uuid = 'nps:places_id';
       SELECT
         changesets.user_id
       FROM
@@ -60,65 +65,72 @@ CREATE OR REPLACE FUNCTION upsert_way(
     INTO
       v_new_id,
       v_new_version;
-      
-      
-INSERT INTO
-   ways (
-     way_id,
-     changeset_id,
-     timestamp,
-     version,
-     visible,
-     redaction_id
-   ) VALUES (
-     v_new_id,
-     v_changeset,
-     v_timestamp,
-     v_new_version,
-     v_visible,
-     v_redaction_id
-   );
- -- Tags
- INSERT INTO
-   way_tags (
-   SELECT
-     v_new_id AS way_id,
-     k,
-     v,
-     v_new_version AS version
-   FROM
-     json_populate_recordset(
-       null::way_tags,
-       v_tags
-     )
-   );
-   
-    IF length(v_uuid) > 0 AND v_new_version = 1 THEN
+
+    -- uuid
+      v_uuid := nps_get_value(v_uuid_field, v_tags);
+      IF v_uuid IS NULL THEN
+       SELECT
+         nps_update_value(v_uuid_field, uuid_generate_v4()::text, v_tags)
+       INTO
+         v_tags;
+      END IF;
+
+    -- Unit Code
+      v_unitcode := nps_get_value(v_unitcode_field, v_tags);
+      IF v_unitcode IS NULL THEN
+       SELECT
+         nps_update_value(v_unitcode_field, nps_get_unitcode(nps_get_way_center(v_nodes)), v_tags)
+       INTO
+         v_tags;
+      END IF;
+    
+    -- Insert into the ways table  
+    INSERT INTO
+     ways (
+       way_id,
+       changeset_id,
+       timestamp,
+       version,
+       visible,
+       redaction_id
+     ) VALUES (
+       v_new_id,
+       v_changeset,
+       v_timestamp,
+       v_new_version,
+       v_visible,
+       v_redaction_id
+     );
+
+    -- Tags
      INSERT INTO
        way_tags (
        SELECT
          v_new_id AS way_id,
-         v_uuid as k,
-         uuid_generate_v4() as v,
+         k,
+         v,
          v_new_version AS version
+       FROM
+         json_populate_recordset(
+           null::way_tags,
+           v_tags
+         )
        );
-       SELECT tag FROM api_current_ways WHERE id = v_new_id INTO v_tags;
-    END IF;
 
-   -- Associated Nodes
-   INSERT INTO
-    way_nodes (
-    SELECT
-      v_new_id AS way_id,
-      node_id as node_id,
-      v_new_version AS version,
-      sequence_id as sequence_id
-    FROM
-      json_populate_recordset(
-        null::way_nodes,
-        v_nodes
-      )
-    );
+    -- Associated Nodes
+     INSERT INTO
+      way_nodes (
+      SELECT
+        v_new_id AS way_id,
+        node_id as node_id,
+        v_new_version AS version,
+        sequence_id as sequence_id
+      FROM
+        json_populate_recordset(
+          null::way_nodes,
+          v_nodes
+        )
+      );
     
     -- Update the pgsnapshot view
     SELECT res FROM nps_dblink_pgs('select * from pgs_upsert_way(' || quote_literal(v_new_id) || ', ' || quote_literal(v_changeset) || ', ' || quote_literal(v_visible) || ', ' || quote_literal(v_timestamp) || ', ' || quote_literal(v_nodes) || ', ' || quote_literal(v_tags) || ', ' || quote_literal(v_new_version) || ', ' || quote_literal(v_user_id) || ')') as res into v_res;
