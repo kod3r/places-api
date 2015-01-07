9905fa74bea599a5ce6821c975d1a20ddab5f71a
Jim McAndrew <jim@loc8.us>
2015-01-06 17:46:42 -0700

    Use a single config and error reporting: Update src/oauth/external.js

--- src/oauth/external.js
+++ src/oauth/external.js
@@ -1,32 +1,31 @@
 /* jshint camelcase: false */
-
-var config = require('../../config'),
-  tools = require('./tools');
-
-module.exports = {
-  accessToken: function(auth, callback) {
-    var auths = tools.splitAuthHeader(auth);
-    tools.getTokenSecret(auths.oauth_token, function(tokenSecret) {
-      tools.osmOauth.getOAuthAccessToken(auths.oauth_token, tokenSecret, function(error, oauthAccessToken, oauthAccessTokenSecret) {
-        if (error) {
-          callback(error);
-        } else {
-          // Get the user info
-          tools.getUserInfo(auths.oauth_token, tokenSecret, oauthAccessToken, oauthAccessTokenSecret, function(e, username, userId) {
-            callback(e, oauthAccessToken, oauthAccessTokenSecret, username, userId);
-          });
-        }
+module.exports = function(config) {
+var tools = require('./tools')(config);
+  return {
+    accessToken: function(auth, callback) {
+      var auths = tools.splitAuthHeader(auth);
+      tools.getTokenSecret(auths.oauth_token, function(tokenSecret) {
+        tools.osmOauth.getOAuthAccessToken(auths.oauth_token, tokenSecret, function(error, oauthAccessToken, oauthAccessTokenSecret) {
+          if (error) {
+            callback(error);
+          } else {
+            // Get the user info
+            tools.getUserInfo(auths.oauth_token, tokenSecret, oauthAccessToken, oauthAccessTokenSecret, function(e, username, userId) {
+              callback(e, oauthAccessToken, oauthAccessTokenSecret, username, userId);
+            });
+          }
+        });
       });
-    });
-  },
-  requestToken: function(callback) {
-    tools.osmOauth.getOAuthRequestToken(function(error, token, tokenSecret) {
-      tools.addRequestToken(token, tokenSecret, function() {
-        callback(error, token, tokenSecret);
+    },
+    requestToken: function(callback) {
+      tools.osmOauth.getOAuthRequestToken(function(error, token, tokenSecret) {
+        tools.addRequestToken(token, tokenSecret, function() {
+          callback(error, token, tokenSecret);
+        });
       });
-    });
-  },
-  authorize: function(req, res) {
-    res.redirect('http://' + config.oauth.server + req.originalUrl);
-  }
+    },
+    authorize: function(req, res) {
+      res.redirect('http://' + config.oauth.server + req.originalUrl);
+    }
+  };
 };
