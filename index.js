6387112644911d58c5a7cdad47e76853707ca5eb
Jim McAndrew <jim@loc8.us>
2015-01-06 17:46:15 -0700

    Use a single config and error reporting: Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,63 @@
 var express = require('express'),
   bodyParser = require('body-parser'),
-  config = require('./config'),
-  apiXapi = require('./lib/apis/xapi'),
-  apiData = require('./lib/apis/data'),
-  oauth = require('./lib/oauth/paths'),
-  allowXSS = require('./lib/allowXSS');
-// Set the environment variables
-//
-exports.routes = function() {
-  var router = express.Router(),
-    poiApp = require('./lib/apiWrapper')(router);
-  // From http://wiki.openstreetmap.org/wiki/API_v0.6#General_information
-  // Allow external webpages to read our JavaScript
-  allowXSS(router);
+  allowXSS = require('./src/allowXSS');
 
-  // API Calls
-  apiXapi.map(function(apiCall) {
-    poiApp.allow(apiCall.method, apiCall.path, '0.6', apiCall.format, apiCall.auth, apiCall.process);
-  });
-
-  // Data Calls
-  apiData.map(function(apiCall) {
-    poiApp.allow(apiCall.method, apiCall.path, 'data', apiCall.format, apiCall.auth, apiCall.process);
-  });
-
-  // Overall capabilities
-  poiApp.allow('GET', 'capabilities', null, null, null, function(req, res) {
-    res.send({
-      'api': config.capabilities
-    });
-  });
-
-  return router;
+var errorWrapper = function(errorFunction) {
+  return function(err, req, res, next) {
+    console.log('ERROR', err);
+    errorFunction(err, req, res, next);
+  };
 };
 
-exports.oauth = function() {
-  var router = express.Router();
+module.exports = function(config) {
 
-  // Return the oauth calls
-  oauth.map(function(oauthCall) {
-    router[(oauthCall.method).toLowerCase()](oauthCall.path, bodyParser.json(), oauthCall.process);
-  });
+  config = config || require('./config');
+  var apiData = require('./src/apis/data')(config),
+    apiXapi = require('./src/apis/xapi')(config),
+    oauth = require('./src/oauth/paths')(config);
 
-  return router;
+  return {
+    'routes': function() {
+      var router = express.Router(),
+        placesApp = require('./src/apiWrapper')(router, config);
+      // From http://wiki.openstreetmap.org/wiki/API_v0.6#General_information
+      // Allow external webpages to read our JavaScript
+      allowXSS(router);
+
+      // API Calls
+      apiXapi.map(function(apiCall) {
+        placesApp.allow(apiCall.method, apiCall.path, '0.6', apiCall.format, apiCall.auth, apiCall.process);
+      });
+
+      // Data Calls
+      apiData.map(function(apiCall) {
+        placesApp.allow(apiCall.method, apiCall.path, 'data', apiCall.format, apiCall.auth, apiCall.process);
+      });
+
+      // Overall capabilities
+      placesApp.allow('GET', 'capabilities', null, null, null, function(req, res) {
+        res.send({
+          'api': config.capabilities
+        });
+      });
+
+      router.use(errorWrapper(placesApp.onError));
+      return router;
+    },
+    'oauth': function() {
+      var router = express.Router();
+
+      // Return the oauth calls
+      oauth.map(function(oauthCall) {
+        router[(oauthCall.method).toLowerCase()](oauthCall.path, bodyParser.json(), oauthCall.process);
+      });
+
+      router.use(errorWrapper(function(err, res){
+        res.set('Content-Type', 'text/html');
+        res.status(500).send(JSON.stringify(err, null, 2));
+      }));
+
+      return router;
+    }
+  };
 };
