518e5b089045469f87403a8c20b6a12ab47e90ae
Jim McAndrew <jim@loc8.us>
2015-05-01 12:52:52 -0600

    min and max bounds in the changeset

--- scripts/sql_scripts/api/func_close_changeset.sql
+++ scripts/sql_scripts/api/func_close_changeset.sql
@@ -17,11 +17,15 @@ CREATE OR REPLACE FUNCTION close_changeset(
     "num_changes" = (
       SELECT sum(counts.count) FROM (
         SELECT count(*) FROM nodes WHERE changeset_id = changesets.id
-        UNION
+        UNION ALL
         SELECT count(*) FROM ways WHERE changeset_id = changesets.id
-        UNION
+        UNION ALL
         SELECT count(*) FROM relations WHERE changeset_id = changesets.id
-      ) counts)
+      ) counts),
+      min_lat = (SELECT MIN(latitude) FROM nodes WHERE changeset_id = changesets.id),
+      max_lat = (SELECT MAX(latitude) FROM nodes WHERE changeset_id = changesets.id),
+      min_lon = (SELECT MIN(longitude) FROM nodes WHERE changeset_id = changesets.id),
+      max_lon = (SELECT MAX(longitude) FROM nodes WHERE changeset_id = changesets.id)
   WHERE
     "id" = v_changeset_id;
   
