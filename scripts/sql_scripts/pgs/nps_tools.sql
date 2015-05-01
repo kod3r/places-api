e76e7dd56304c298dcec138e0a63d0d269362744
Jim McAndrew <jim@loc8.us>
2015-05-01 11:16:54 -0600

    Add foreign data wrappers for new rendering

--- scripts/sql_scripts/pgs/nps_tools.sql
+++ scripts/sql_scripts/pgs/nps_tools.sql
@@ -755,3 +755,23 @@ $BODY$
   COST 100;
 ALTER FUNCTION public.nps_pgs_update_o2p(bigint, character)
   OWNER TO osm;
+
+---------------------------
+-- Foreign Data
+---------------------------
+CREATE EXTENSION postgres_fdw;
+CREATE SERVER places_api FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'localhost', port '5432', dbname 'places_api');
+CREATE USER MAPPING FOR PUBLIC SERVER places_api OPTIONS (user 'postgres', password 'postgres');
+
+
+--DROP FOREIGN TABLE api_changesets;
+CREATE FOREIGN TABLE api_changesets (id bigint, closed_at timestamp without time zone) SERVER places_api OPTIONS (table_name 'changesets');
+--DROP FOREIGN TABLE api_nodes;
+CREATE FOREIGN TABLE api_nodes (id bigint, visible boolean, version bigint, changeset bigint, "timestamp" timestamp without time zone, "user" text, uid bigint, lat double precision, lon double precision, tag JSON) SERVER places_api OPTIONS (table_name 'pgs_current_nodes');
+--DROP FOREIGN TABLE api_ways;
+CREATE FOREIGN TABLE api_ways (id bigint, visible boolean, version bigint, changeset bigint, "timestamp" timestamp without time zone, "user" text, "uid" bigint, nd JSON, tag JSON)  SERVER places_api OPTIONS (table_name 'pgs_current_ways');
+--DROP FOREIGN TABLE api_relations;
+CREATE FOREIGN TABLE api_relations (id bigint, visible boolean, version bigint, changeset bigint, "timestamp" timestamp without time zone, "user" text, "uid" bigint, member JSON, tag JSON) SERVER places_api OPTIONS (table_name 'pgs_current_relations');
+--DROP FOREIGN TABLE api_users;
+CREATE FOREIGN TABLE api_users (email character varying (255), id bigint, display_name character varying (255)) SERVER places_api OPTIONS (table_name 'users');
+
